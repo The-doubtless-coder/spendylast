936acf49c91af4c46b65524ca7c7c1e4126964ec
ian <ianwanjala4@gmail.com>
2022-07-25 16:58:59 +0300

    push only yml ignore properties, added only postgre ignore sql---but will change to master

--- src/main/java/com/spenndify/application/spendylast/onboarding/login/AuthController.java
+++ src/main/java/com/spenndify/application/spendylast/onboarding/login/AuthController.java
@@ -29,6 +29,7 @@ public class AuthController {
                 "Kindly recheck and try again");
         }
         //todo: unlock account
+
         return jwtUtil.generateToken(authRequest.getUserName());
     }
 }
