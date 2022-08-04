e2deae39ea12c4e784d4dcff22b040fa606168bb
wanjalize-Backend <ianwanjala4@gmail.com>
2022-08-04 08:24:28 +0300

    new cors beans

--- src/main/java/com/spenndify/application/spendylast/onboarding/websecurity/configsec/WebSecurityConfig.java
+++ src/main/java/com/spenndify/application/spendylast/onboarding/websecurity/configsec/WebSecurityConfig.java
@@ -2,15 +2,18 @@ package com.spenndify.application.spendylast.onboarding.websecurity.configsec;
 
 import com.spenndify.application.spendylast.onboarding.jwts.JwtFilter;
 import com.spenndify.application.spendylast.onboarding.spendyuser.SpendyService;
+import com.spenndify.application.spendylast.onboarding.websecurity.cors.SecurityBean;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -23,11 +26,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final SpendyService spendyService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private JwtFilter jwtFilter;//enable filter and register filter
+    private SecurityBean securityBean;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-                .and()
+        http.cors((Customizer<CorsConfigurer<HttpSecurity>>) securityBean)
+//                .and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/spendy/user/**").permitAll()
