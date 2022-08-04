5f559023a0d824231ed1a91514615cb9e30399ae
wanjalize-Backend <ianwanjala4@gmail.com>
2022-08-04 09:32:04 +0300

    new cors beans

--- src/main/java/com/spenndify/application/spendylast/onboarding/websecurity/configsec/WebSecurityConfig.java
+++ src/main/java/com/spenndify/application/spendylast/onboarding/websecurity/configsec/WebSecurityConfig.java
@@ -26,8 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-                .and()
+        http
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/spendy/user/**").permitAll()
