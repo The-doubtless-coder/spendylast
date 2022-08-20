c09c500762dcc22c00b99eda06998dc115cc8ba2
topcoder <ianwanjala4@gmail.com>
2022-08-20 22:09:42 +0300

    CORS USING WEBCONFIGURER

--- src/main/java/com/spenndify/application/spendylast/corsconfig/CorsConfig.java
+++ src/main/java/com/spenndify/application/spendylast/corsconfig/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.spenndify.application.spendylast.corsconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,13 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+    @Value("${allowed.origin}")
+    private String allowedOrigin;
     @Bean
     public WebMvcConfigurer getCorsConfiguration(){
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns(allowedOrigin)
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
--- src/main/java/com/spenndify/application/spendylast/onboarding/Twilio/verifyotp/VerifyRequest.java
+++ src/main/java/com/spenndify/application/spendylast/onboarding/Twilio/verifyotp/VerifyRequest.java
@@ -3,7 +3,6 @@ package com.spenndify.application.spendylast.onboarding.Twilio.verifyotp;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
 
 @Data
 @NoArgsConstructor
